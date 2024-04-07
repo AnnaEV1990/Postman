36be96fec4a3f4223cce741afdf5bf5c0c0f38a8
Anna Evseykina <anytta9032@yandex.ru>
2024-04-07 20:15:05 +0700

    change Test-file

---
+++ src/test/java/MobileBankApiTestV1.java
@@ -0,0 +1,23 @@
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class MobileBankApiTestV1 {
+    @Test
+    public void shouldReturnDemoAccounts() {
+
+                given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", Matchers.equalTo("some value"));
+
+
+    }
+
+}
